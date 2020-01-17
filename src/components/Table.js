f07a734079256f2a79e4dde297b15a5a462550ce
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-17 17:01:08 -0300

    problemas com a interface

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,53 +1,113 @@
 import React, { Component } from 'react';
-import 'rbx/index.css';
-import { Hero, Title, Table as TB } from 'rbx';
-
+import { connect } from 'react-redux';
+// import 'rbx/index.css';
+// import { Hero, Title, Table as TB } from 'rbx';
+import { planetOfStarWar } from '../actions';
 // import PropTypes from 'prop-types';
 
-// import { connect } from 'react-redux';
-
-export default class Table extends Component {
-  // static propTypes = {
-  //   prop: PropTypes
-  // }
+// const table = () => {
+//   <table>
+//   <tr>
+//     <th>{}</th>
+//     <th>{}</th>
+//     <th>{}</th>
+//     <th>{}</th>
+//     <th>{}</th>
+//     <th>{}</th>
+//     <th>{}</th>
+//     <th>{}</th>
+//   </tr>
+//   <tr>
+//     <td>{}</td>
+//     <td>{}</td>
+//     <td>{}</td>
+//     <td>{}</td>
+//     <td>{}</td>
+//     <td>{}</td>
+//     <td>{}</td>
+//     <td>{}</td>
+//   </tr>
+// </table>
+class Table extends Component {
+  componentDidMount() {
+    const { getPlanetFilter } = this.props;
+    getPlanetFilter();
+  }
 
   render() {
+    const list = [
+      'rotation_period',
+      'orbital_period',
+      'diameter',
+      'climate',
+      'gravity',
+      'terrain',
+      'surface_water',
+      'population',
+    ];
+    const {
+      rotation_period,
+      orbital_period,
+      diameter,
+      climate,
+      gravity,
+      terrain,
+      surface_water,
+      population,
+    } = this.props;
     return (
-      <div>
-        <Hero color='dark'>
-          <Hero.Body>
-            <Title>
-              <Title>StarWars DataTB with Filters</Title>
-            </Title>
-          </Hero.Body>
-        </Hero>
-        <TB bordered>
-          <TB.Head>
-            <TB.Row>
-              <TB.Heading>One</TB.Heading>
-              <TB.Heading>Two</TB.Heading>
-            </TB.Row>
-          </TB.Head>
-          <TB.Body>
-            <TB.Row>
-              <TB.Cell>Three</TB.Cell>
-              <TB.Cell>Four</TB.Cell>
-            </TB.Row>
-          </TB.Body>
-          <TB.Body>
-            <TB.Row>
-              <TB.Cell>Five</TB.Cell>
-              <TB.Cell>Six</TB.Cell>
-            </TB.Row>
-          </TB.Body>
-        </TB>
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <th>{list[0]}</th>
+            <th>{list[1]}</th>
+            <th>{list[2]}</th>
+            <th>{list[3]}</th>
+            <th>{list[4]}</th>
+            <th>{list[5]}</th>
+            <th>{list[6]}</th>
+            <th>{list[7]}</th>
+          </tr>
+          <tr>
+            <td>{rotation_period}</td>
+            <td>{orbital_period}</td>
+            <td>{diameter}</td>
+            <td>{climate}</td>
+            <td>{gravity}</td>
+            <td>{terrain}</td>
+            <td>{surface_water}</td>
+            <td>{population}</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({});
+const mapStateToProps = ({
+  allPlanetWar: {
+    rotation_period,
+    orbital_period,
+    diameter,
+    climate,
+    gravity,
+    terrain,
+    surface_water,
+    population,
+  },
+}) => ({
+  rotation_period,
+  orbital_period,
+  diameter,
+  climate,
+  gravity,
+  terrain,
+  surface_water,
+  population,
+});
 
-// const mapDispatchToProps = {};
+const mapDispatchToProps = (dispatch) => ({
+  getPlanetFilter: () => dispatch(planetOfStarWar),
+});
 
-// export default connect(mapStateToProps, mapDispatchToProps)(TB);
+export default connect(mapStateToProps, mapDispatchToProps)(Table);
