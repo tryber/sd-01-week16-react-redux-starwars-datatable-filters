8405bd4eefb8b2863d4177f5a0004c1b910ca673
antoniopaolillo <antonio_francisco_p@live.com>
2020-01-19 13:47:34 -0300

    alterando maneira de filtrar tabela para via button

--- src/components/ValuesInput.js
+++ src/components/ValuesInput.js
@@ -3,15 +3,13 @@ import { connect } from "react-redux";
 import { updatingValuesFilter } from "../actions/valuesFilter";
 
 class ValuesInput extends React.Component {
-  componentDidMount() {
-    const obj = {
-      numeric_values: {
-        column: this.arrayOfColumns()[0],
-        comparison: "Maior",
-        value: ""
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      column: this.arrayOfColumns()[0],
+      comparison: "Maior",
+      value: ""
     };
-    this.props.updateValues(obj);
   }
 
   arrayOfColumns() {
@@ -26,72 +24,58 @@ class ValuesInput extends React.Component {
 
   generateColumnOptions() {
     return this.arrayOfColumns().map(each => {
-      return <option key={each}value={each}>{each}</option>;
+      return (
+        <option key={each} value={each}>
+          {each}
+        </option>
+      );
     });
   }
 
-  changeStateColumn(event) {
-    const {
-      numeric_values: { comparison, value }
-    } = this.props.filters;
-    const obj = {
-      numeric_values: {
-        column: event.target.value,
-        comparison,
-        value
-      }
-    };
-    return this.props.updateValues(obj);
-  }
-
-  changeStateComparison(event) {
-    const {
-      numeric_values: { column, value }
-    } = this.props.filters;
-    const obj = {
-      numeric_values: {
-        column,
-        comparison: event.target.value,
-        value
-      }
-    };
-    return this.props.updateValues(obj);
-  }
-
-  changeStateValue(event) {
-    const {
-      numeric_values: { column, comparison }
-    } = this.props.filters;
+  updateStore(state) {
+    const { column, comparison, value } = state;
     const obj = {
       numeric_values: {
         column,
         comparison,
-        value: event.target.value
+        value
       }
     };
-    return this.props.updateValues(obj);
+    this.props.updateValues(obj);
+  }
+
+  changeState(event, id) {
+    this.setState({
+      [id]: event.target.value
+    });
   }
 
   render() {
     return (
       <div>
+        Choose the column to filter:
         <label>
-          Choose the column to filter:
-          <select onChange={e => this.changeStateColumn(e)} id="column">
+          <select onChange={e => this.changeState(e, "column")} id="column">
             {this.generateColumnOptions()}
           </select>
         </label>
-        <select onChange={e => this.changeStateComparison(e)} id="comparison">
+        <select
+          onChange={e => this.changeState(e, "comparison")}
+          id="comparison"
+        >
           <option value="Maior">Maior que</option>
           <option value="Menor">Menor que</option>
           <option value="Igual">Igual</option>
         </select>
         <input
-          onChange={e => this.changeStateValue(e)}
+          onChange={e => this.changeState(e, "value")}
           id="comparisonValue"
           type="number"
           placeholder="Valor"
         />
+        <button onClick={() => this.updateStore(this.state)}>
+          Adicionar filtro
+        </button>
       </div>
     );
   }
