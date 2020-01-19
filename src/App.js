6ccab617c924a247595f6ddea34adeb8c06ba0f5
richardwellerson <richardwellerson@gmail.com>
2020-01-19 16:53:51 -0300

    Separando os components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import loadingAPI from './components/API';
 import Table from './components/Table';
 
 function App() {
+  console.log(loadingAPI);
   return (
     <div className="App">
       <header className="App-header">
