4c0b9a7ea23fb8c233b4a18d4ebc7c9ea1c6929d
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-01-19 19:41:16 -0300

    mudanças drasticas...

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPlanets } from '../actions/apiAndRequests';
-
 import Filter from './Filter';
 
 class Table extends Component {
