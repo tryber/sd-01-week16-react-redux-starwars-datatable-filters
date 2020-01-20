623ac0546da0381e2348e3120994fd999c61e4f1
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-20 10:39:52 -0200

    Code Climate

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,9 +4,7 @@ import filterText from '../actions/FilterText';
 import FilterNum from './FilterNum';
 
 class Filter extends Component {
-
   render() {
-    const { categorys, value, column, comparison } = this.props;
     return (
       <div>
         <input type="text" onChange={(e) => this.props.filterText(e.target.value)}>
@@ -17,27 +15,8 @@ class Filter extends Component {
   }
 }
 
-const mapStateToProps = ({
-  filterPlanets: {
-    filters: { numericValues: {
-      column,
-      comparison,
-      value,
-    },
-      categorys,
-    }
-  }
-}) => (
-    {
-      categorys,
-      value,
-      column,
-      comparison,
-    }
-  );
-
 const mapDispatchToProps = (dispatch) => ({
   filterText: (text) => dispatch(filterText(text)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+export default connect(null, mapDispatchToProps)(Filter);
