c7de4feb987886dc7bd1be2a1925a54da2005387
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-20 11:53:10 -0200

    Body Statment

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -57,21 +57,19 @@ class Table extends Component {
           <th>rotation period</th>
           <th>surface water</th>
         </tr>
-        {filterPlanets.map((planet) => {
-          return (
-            <tr>
-              <td>{planet.name}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.population}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.surface_water}</td>
-            </tr>
-          );
-        })}
+        {filterPlanets.map(planet => (
+          <tr>
+            <td>{planet.name}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.population}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.surface_water}</td>
+          </tr>
+        ))}
       </table>
     );
   }
