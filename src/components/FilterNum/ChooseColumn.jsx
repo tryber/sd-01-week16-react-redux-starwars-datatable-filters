94be30fc94bb21b1c812a05161ade38d91b858c4
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-28 17:29:18 -0200

    Code Climate Choose Column Filter Num

--- src/components/FilterNum/ChooseColumn.jsx
+++ src/components/FilterNum/ChooseColumn.jsx
@@ -9,11 +9,13 @@ class ChooseColumn extends Component {
     const { available_categories, dispatchSomething } = this.props;
     if (available_categories.length >= 1) {
       return (
-        <select key="categories" onChange={(e) => {
-          if (e.target.value !== 'none') {
-            dispatchSomething(filterNumberColumn, e.target.value)
-          }
-        }}>
+        <select
+          key="categories" onChange={(e) => {
+            if (e.target.value !== 'none') {
+              dispatchSomething(filterNumberColumn, e.target.value);
+            }
+          }}
+        >
           <option value="none" >Choose Column</option>
           {available_categories.map((category) => (
             <option key={category} value={category} >{category}</option>
@@ -45,6 +47,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 ChooseColumn.propTypes = {
   dispatchSomething: PropTypes.func.isRequired,
+  available_categories: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChooseColumn);
