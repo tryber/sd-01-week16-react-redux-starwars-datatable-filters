aac00505c346c8270946c6dacb885533913d024d
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-28 18:23:46 -0200

    A Different King Of Human

--- src/actions/filters.jsx
+++ src/actions/filters.jsx
@@ -10,50 +10,50 @@ export const CHANGE_ORDER = 'CHANGE_ORDER';
 export const CHANGE_COLUMN = 'CHANGE_COLUMN';
 
 export const filterText = (name) => ({
-    type: FILTER_TEXT,
-    name,
+  type: FILTER_TEXT,
+  name,
 });
 
 export const filterNumberColumn = (column) => ({
-    type: FILTER_NUMBER_COLUMN,
-    column,
+  type: FILTER_NUMBER_COLUMN,
+  column,
 });
 
 export const filterNumberComparison = (comparison) => ({
-    type: FILTER_NUMBER_COMPARISON,
-    comparison,
+  type: FILTER_NUMBER_COMPARISON,
+  comparison,
 });
 
 export const filterNumberValue = (value) => ({
-    type: FILTER_NUMBER_VALUE,
-    value,
+  type: FILTER_NUMBER_VALUE,
+  value,
 });
 
 export const addFilter = (node) => ({
-    type: ADD_FILTER,
-    node,
+  type: ADD_FILTER,
+  node,
 });
 
 export const resetFilter = () => ({
-    type: RESET_FILTER,
+  type: RESET_FILTER,
 });
 
 export const clearOneFilter = (node) => ({
-    type: CLEAR_ONE_FILTER,
-    node,
+  type: CLEAR_ONE_FILTER,
+  node,
 });
 
 export const modifyCategories = (node) => ({
-    type: MODIFY_AVAILABLE_CATEGORY,
-    node,
+  type: MODIFY_AVAILABLE_CATEGORY,
+  node,
 });
 
 export const changeOrder = (order) => ({
-    type: CHANGE_ORDER,
-    order,
+  type: CHANGE_ORDER,
+  order,
 });
 
 export const changeColumn = (column) => ({
-    type: CHANGE_COLUMN,
-    column,
+  type: CHANGE_COLUMN,
+  column,
 });
