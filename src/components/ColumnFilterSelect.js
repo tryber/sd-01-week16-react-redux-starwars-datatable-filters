96d8446f2af361dcccbdafa1e7acd504d0cfcc45
Guilherme Luiz <guiiluiz44@gmail.com>
2020-01-30 14:53:02 -0300

    filters are uniques per column

--- src/components/ColumnFilterSelect.js
+++ src/components/ColumnFilterSelect.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-function ColumnFilterSelect({ handleChange }) {
+function verifySelect(filters, value) {
+  const exists = filters.numeric_values.find((filterObj) => filterObj.column === value);
+  if (exists) return false;
+  return true;
+}
+
+function ColumnFilterSelect({ numeric_values, handleChange }) {
   return (
     <select name="column" onChange={(e) => handleChange(e.target.value)}>
       <option value="">Selecionar Opção</option>
-      <option value="population">População</option>
-      <option value="orbital_period">Duração Orbital</option>
-      <option value="diameter">Diâmetro</option>
-      <option value="rotation_period">Duração da Rotação</option>
-      <option value="surface_water">Superfície da Água</option>
+      {verifySelect(numeric_values, 'population') && <option value="population">População</option>}
+      {verifySelect(numeric_values, 'orbital_period') && <option value="orbital_period">Duração Orbital</option>}
+      {verifySelect(numeric_values, 'diameter') && <option value="diameter">Diâmetro</option>}
+      {verifySelect(numeric_values, 'rotation_period') && <option value="rotation_period">Duração da Rotação</option>}
+      {verifySelect(numeric_values, 'surface_water') && <option value="surface_water">Superfície da Água</option>}
     </select>
   );
 }
@@ -18,4 +25,6 @@ ColumnFilterSelect.propTypes = {
   handleChange: PropTypes.func.isRequired,
 };
 
-export default ColumnFilterSelect;
+const mapStateToProps = ({ filters: numeric_values }) => ({ numeric_values });
+
+export default connect(mapStateToProps)(ColumnFilterSelect);
