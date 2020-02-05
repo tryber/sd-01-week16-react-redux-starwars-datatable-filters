7f792f3f1051ad43052bf460a28ba8e5682abd69
Guilherme Luiz <guiiluiz44@gmail.com>
2020-02-05 15:06:00 -0300

    if all filters are already set, should not load any other

--- src/components/ComparisonFilterSelect.js
+++ src/components/ComparisonFilterSelect.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function ComparisonFilterSelect({ handleChange }) {
   return (
     <select name="type" onChange={(e) => handleChange(e.target.value)}>
-      <option value="">Selecionar Opção</option>
+      <option selected disabled>Selecionar Opção</option>
       <option value="bigger">Maior que</option>
       <option value="less">Menor que</option>
       <option value="equal">Igual a</option>
