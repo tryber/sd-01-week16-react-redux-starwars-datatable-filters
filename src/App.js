ca6c16bfaf0ca3cf97806c8146c8faf3139688e9
Mateus Talles <mateustalles@gmail.com>
2020-03-01 17:44:22 -0300

    First pre-requisite done!

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import { Provider } from 'react-redux';
 import store from './store/index';
 import Table from './components/Table';
 import SWAPI from './services/SWAPI';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <header className="App-header">
-          <SWAPI />
-          <Table />
-        </header>
+        <SWAPI />
+        <Table />
       </Provider>
     </div>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,33 +1,58 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import '../style/Table.css';
+
+const tableHeaders = () => (
+  <tr>
+    <th>Name</th>
+    <th>Rotation period</th>
+    <th>Orbital period</th>
+    <th>Diamater</th>
+    <th>Climate</th>
+    <th>Gravity</th>
+    <th>Terrain</th>
+    <th>Surface Water</th>
+    <th>Population</th>
+    <th>Films</th>
+    <th>Created</th>
+    <th>Edited</th>
+    <th>URL</th>
+  </tr>
+);
 
 const Table = ({ table }) => {
   return (
     <div>
-      <div>StarWars Datatable with Filters</div>
-      <div>
-        { table.map((
-          { name, rotation_period, orbital_period, diameter, climate, gravity, terrain, surface_water, population, films, created, edited, url }
-        ) =>{
-          <form>
-            <tr>
-              <th>Name</th>
-              <th>Rotation period</th>
-              <th>Orbital period</th>
-              <th>Diamater</th>
-              <th>Climate</th>
-              <th>Gravity</th>
-              <th>Terrain</th>
-              <th>Surface Water</th>
-              <th>Population</th>
-              <th>Films</th>
-              <th>Created</th>
-              <th>Edited</th>
-              <th>URL</th>
-            </tr>
-
-          </form>
-        } planet.name) }
+      <h1>StarWars Datatable with Filters</h1>
+      <div className="table-container">
+        <table className="table">
+          <thead>
+            {tableHeaders()}
+          </thead>
+          <tbody>
+            {table.map(({
+              name, rotation_period: rotationPeriod, orbital_period: orbitalPeriod, diameter,
+              climate, gravity, terrain, surface_water: surfaceWater, population, films, created,
+              edited, url,
+            }) => (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>{rotationPeriod}</td>
+                <td>{orbitalPeriod}</td>
+                <td>{diameter}</td>
+                <td>{climate}</td>
+                <td>{gravity}</td>
+                <td>{terrain}</td>
+                <td>{surfaceWater}</td>
+                <td>{population}</td>
+                <td>{films}</td>
+                <td>{created}</td>
+                <td>{edited}</td>
+                <td>{url}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
