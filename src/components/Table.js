b3f23ac8c943cc9bb3a598822690644e8446b436
Mateus Talles <mateustalles@gmail.com>
2020-03-01 17:05:59 -0300

    Start to create table

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,11 +6,32 @@ const Table = ({ table }) => {
     <div>
       <div>StarWars Datatable with Filters</div>
       <div>
-        { table.map((planet) => planet.name) }
+        { table.map((
+          { name, rotation_period, orbital_period, diameter, climate, gravity, terrain, surface_water, population, films, created, edited, url }
+        ) =>{
+          <form>
+            <tr>
+              <th>Name</th>
+              <th>Rotation period</th>
+              <th>Orbital period</th>
+              <th>Diamater</th>
+              <th>Climate</th>
+              <th>Gravity</th>
+              <th>Terrain</th>
+              <th>Surface Water</th>
+              <th>Population</th>
+              <th>Films</th>
+              <th>Created</th>
+              <th>Edited</th>
+              <th>URL</th>
+            </tr>
+
+          </form>
+        } planet.name) }
       </div>
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => ({
   table: state.data,
